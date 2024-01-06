6d9112fc8f9f212af8230de1d27bdf31de5b12e7
Abdur Rahim <abdur.rahim@enosisbd.com>
2024-01-06 10:04:24 +0600

    Added opentelemetry to the reverseproxy.

--- reverse-proxy/ReverseProxy.BFF/Program.cs
+++ reverse-proxy/ReverseProxy.BFF/Program.cs
@@ -1,13 +1,25 @@
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddReverseProxy()
         .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+    builder.Services.AddOpenTelemetry()
+            .WithMetrics(builder => builder
+                .AddAspNetCoreInstrumentation()
+                .AddRuntimeInstrumentation()
+                .AddPrometheusExporter(config =>
+                {
+                    config.ScrapeResponseCacheDurationMilliseconds = 1000;
+                }));
 }
 
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
     app.MapReverseProxy();
+    app.MapPrometheusScrapingEndpoint();
     app.UseRouting();
     app.Run();
 }
