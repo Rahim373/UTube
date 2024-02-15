8c879c0526babcd93bfc3f953c5dc547afc8b664
Abdur Rahim <abdur.rahim@enosisbd.com>
2024-02-15 13:26:40 +0600

    Added registration option in identity service.

--- identity-service/IdentityService.Web/Controllers/AuthenticationController.cs
+++ identity-service/IdentityService.Web/Controllers/AuthenticationController.cs
@@ -21,6 +21,33 @@ public class AuthenticationController : Controller
         _signInManager = signInManager;
     }
 
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var createdUser = await _userManager.CreateAsync(new ApplicationUser
+            {
+                Email = model.Email.Trim(),
+                UserName = model.Email.Trim(),
+                ChannelName = model.ChannelName?.Trim(),
+            }, model.Password);
+
+            if (createdUser.Succeeded)
+            {
+                return RedirectToAction("Login", new { model.ReturnUrl });
+            }
+        }
+
+        return View(model);
+    }
+
+
     public IActionResult Login()
     {
         return View();
@@ -30,11 +57,18 @@ public class AuthenticationController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Login(LoginViewModel model)
     {
+        if (!ModelState.IsValid)
+        {
+            ViewData["Errors"] = "Incorrect email or password";
+            return View(model);
+        }
+
         var signInResult = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
 
         if (!signInResult.Succeeded)
         {
-            return Unauthorized();
+            ViewData["Errors"] = "Incorrect email or password";
+            return View(model);
         }
 
         var user = await _userManager.FindByEmailAsync(model.Email);
---
+++ identity-service/IdentityService.Web/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Web.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [Required]
+    [Display(Name = "Channel Name")]
+    public string ChannelName { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+
+    public string ReturnUrl { get; set; }
+}
